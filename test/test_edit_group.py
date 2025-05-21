e635d70a02e0b29280eefbd84aebc2297fe08707
savitskiy1995 <glebsav@bk.ru>
2025-05-22 00:13:03 +0300

    Задание 20. Исправление 2.0

--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -1,16 +1,16 @@
+import random
 from random import randrange
 
 from model.group import Group
 
 def test_edit_group(app, db, check_ui):
     old_groups = db.get_group_list_from_db()
-    index = randrange(len(old_groups))
-    group = Group(name="Edit group", header="Edit header")
-    group.id = old_groups[index].id
-    app.group.edit_group_by_index(group, index)
+    group = random.choice(old_groups)
+    new_group = Group(name="Edit group", header="Edit header")
+    app.group.edit_group_by_index(group, group.id)
     new_groups = db.get_group_list_from_db()
     assert len(old_groups) == len(new_groups)
-    old_groups[index] = group
+    new_group.id = group.id
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
     if check_ui:
         assert sorted(new_groups, key=Group.id_or_max) == sorted(app.group.get_group_list(), key=Group.id_or_max)
